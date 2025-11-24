050be64c838505a1122d0ce57ede45d085b2625b
umanari145 <umanari145@gmail.com>
2025-11-24 11:28:49 +0900

    installを見る

--- service/install.sh
+++ service/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eux
-aws s3 cp s3://original-apis/.env.production .env
+aws s3 cp s3://original-apis/.env.production .env --profile for_awslightsail
 npm install
 npx prisma migrate dev
 npm run build
