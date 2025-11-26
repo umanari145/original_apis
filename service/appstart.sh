6c9b5e001fe4d816252ac26789097f96af1a0777
umanari145 <umanari145@gmail.com>
2025-11-26 21:30:33 +0900

    installを実行

--- service/appstart.sh
+++ service/appstart.sh
@@ -1,4 +1,5 @@
 
 #!/bin/bash
 set -eux
+echo "------ restart---------------"
 pm2 restart /opt/bitnami/app/dist/server.js >> /opt/bitnami/deploy.log
--- service/install.sh
+++ service/install.sh
@@ -2,6 +2,7 @@
 set -eux
 cd /opt/bitnami/app
 aws s3 cp s3://original-apis/.env.production .env --profile for_awslightsail
+echo "------ install---------------"
 npm install >> /opt/bitnami/deploy.log
 npx prisma migrate dev >> /opt/bitnami/deploy.log
 npm run build >> /opt/bitnami/deploy.log
