d7a3c549b59ee0acf77a722b200585ea2bb563c9
umanari145 <umanari145@gmail.com>
2025-11-26 21:35:42 +0900

    パスを修正

--- service/appstart.sh
+++ service/appstart.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 set -eux
 echo "------ restart---------------"
-pm2 restart /opt/bitnami/app/dist/server.js >> /opt/bitnami/deploy.log
+pm2 restart /opt/bitnami/app/dist/server.js >> /opt/bitnami/app/deploy.log
--- service/install.sh
+++ service/install.sh
@@ -3,6 +3,6 @@ set -eux
 cd /opt/bitnami/app
 aws s3 cp s3://original-apis/.env.production .env --profile for_awslightsail
 echo "------ install---------------"
-npm install >> /opt/bitnami/deploy.log
-npx prisma migrate dev >> /opt/bitnami/deploy.log
-npm run build >> /opt/bitnami/deploy.log
+npm install >> /opt/bitnami/app/deploy.log
+npx prisma migrate dev >> /opt/bitnami/app/deploy.log
+npm run build >> /opt/bitnami/app/deploy.log
